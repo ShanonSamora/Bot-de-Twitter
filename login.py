353f7a2afc7a2311962a3bc2babe4c90c941a104
shanons6 <60020892+shanons6@users.noreply.github.com>
2020-01-30 11:25:38 -0300

    Update login.py

--- login.py
+++ login.py
@@ -1,9 +1,9 @@
 from BotTwitter import BotTwitter
 
 if __name__=='__main__':
-    username="shanonsamora3@gmail.com"
-    password="lokiju12"
-    hashtags = ["drstone", "dr stone"]
+    username=" " #Entre las comillas necesitas poner el email de la cuenta que vas a usar.
+    password=" " #Acá necesitas poner la contraseña.
+    hashtags = [" ", " "] #Acá necesitas poner los hashtags que quieras que el bot likee y siga a los autores de dichos tweets.
 
     bot = BotTwitter(username, password)
     bot.login()
